9c30dfac615c88729d1e9cc5c1ae86145e6b47a0
Md. Yasin Mollah <mymofcl@gmail.com>
2024-07-22 17:58:29 +0600

    Salary Amount Negotiated v.2

--- TestModules/PriceChoice.py
+++ TestModules/PriceChoice.py
@@ -4,10 +4,12 @@ days = int(input("Enter the days :: "))
 
 VariableSalary = 0
 
-for i in range(1,days+1):
+for i in range(1, days+1):
     VariableSalary = VariableSalary + 2**(i-1)
 
 if VariableSalary > FixedSalary:
-    print("I will accept the Variable Salary. And the Amount is $",VariableSalary)
+    print("I will accept the Variable Salary. And the Amount is $", VariableSalary)
 else:
     print("I will accept the Fixed Salary. And the Amount is $", FixedSalary)
+
+print("\nTotal Length :: ", len(str(VariableSalary)))
