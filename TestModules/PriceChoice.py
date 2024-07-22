4de1f822a2ee6149f7f4cd75bea58caa51211f7e
Md. Yasin Mollah <mymofcl@gmail.com>
2024-07-22 17:44:31 +0600

    Salary Amount Negotiated

---
+++ TestModules/PriceChoice.py
@@ -0,0 +1,13 @@
+FixedSalary = 1000000000
+
+days = int(input("Enter the days :: "))
+
+VariableSalary = 0
+
+for i in range(1,days+1):
+    VariableSalary = VariableSalary + 2**(i-1)
+
+if VariableSalary > FixedSalary:
+    print("I will accept the Variable Salary. And the Amount is $",VariableSalary)
+else:
+    print("I will accept the Fixed Salary. And the Amount is $", FixedSalary)
