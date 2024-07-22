95dc8cfe3654294bc255c78c371e02230ac2b27f
Md. Yasin Mollah <mymofcl@gmail.com>
2024-07-22 13:06:36 +0600

    Doing Talbahana

---
+++ TestModules/init.py
@@ -0,0 +1,20 @@
+year = int(input("Enter a year :: "))
+
+# if year % 4 == 0:
+#     if year % 100 == 0:
+#         if year % 400 == 0:
+#             print(year, " is a leap year")
+#     else:
+#         print(year, " is leap year")
+# else:
+#     print(year, "is not a leap year")
+
+if year % 4 != 0:
+    print(year, "is not a leap year")
+else:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print(year, " is a leap year")
+    else:
+        print(year, " is leap year")
+
